3fd9dde684df0f5ffaac58226c9d7a6973445221
MatthewG610 <mgodoy0101@gmail.com>
2025-02-18 15:54:53 -0500

    Update lab5-2.cpp

--- lab5-2.cpp
+++ lab5-2.cpp
@@ -1,12 +1,24 @@
-//lab5-2.cpp - displays the total amount due
-//Created/revised by <your name> on <current date>
-
-#include <iostream>
-#include <iomanip>
-using namespace std;
-
 int main()
 {
+	const double DISCOUNT_RATE = 0.1;
+	 const double SHIP_CHG1 = 0.99;
+	 const double SHIP_CHG2 = 4.99;
+	 double amtOwed = 0.0;
+	 char member = ' ';
 
+	 cout << "Amount owed before any discount and shipping: ";
+	 cin >> amtOwed;
+	 cout << "Premier Club member (Y/N)? ";
+	 cin >> member;
+	 if (toupper(member) == 'Y')
+			 amtOwed -= amtOwed * DISCOUNT_RATE;
+	 if (amtOwed >= 100.0)
+			 amtOwed += SHIP_CHG1;
+	 else
+			 amtOwed += SHIP_CHG2;
+	 cout << fixed << setprecision(2);
+	 cout << "Amount owed after any discount and shipping: "
+			 << amtOwed << endl;
 	return 0;
 }	//end of main function
+‎lab5-3.cpp
