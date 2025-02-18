acb79cb3150b03fe9f378c38727c1a94397c689c
MatthewG610 <mgodoy0101@gmail.com>
2025-02-18 16:32:42 -0500

    Update hw5-12.cpp

--- hw5-12.cpp
+++ hw5-12.cpp
@@ -1,3 +1,10 @@
+//hw5-12.cpp - displays the total owed
+//Created/revised by <your name> on <current date>
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
 int main()
 {
 	double price_1 = 0;
@@ -8,14 +15,22 @@ int main()
 	cout << "Enter price for second product" << endl;
 	cin >> price_2;
 
+
 	if (price_1 > price_2) {
+
 			price_2 = price_2 / 2;
+
 	} else if (price_2 > price_1) {
+
 			price_1 = price_1 / 2;
+
 	} else if (price_2 == price_1) {
+
 			price_2 = price_2 / 2;
 	}
+
 	total_price = price_1 + price_2;
+
 	cout << "Total price is $" << total_price << endl;
 	return 0;
 }	//end of main function
